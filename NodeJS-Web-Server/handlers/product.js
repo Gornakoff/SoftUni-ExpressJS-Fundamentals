7254bfa9deccdd9d8316a393e2e23593c11eefc1
Gornakoff <nikolaygornakov@yahoo.com>
2017-05-24 12:40:38 +0300

    Implement logic to process images

--- NodeJS-Web-Server/handlers/product.js
+++ NodeJS-Web-Server/handlers/product.js
@@ -2,7 +2,7 @@ const url = require('url')
 const database = require('../config/database')
 const fs = require('fs')
 const path = require('path')
-const qs = require('querystring')
+// const qs = require('querystring')
 const multiparty = require('multiparty')
 const shortid = require('shortid')
 
@@ -18,22 +18,50 @@ module.exports = (req, res) => {
       res.end()
     })
   } else if (req.pathname === '/product/add' && req.method === 'POST') {
-    let dataString = ''
+    let product = {}
+    let form = new multiparty.Form()
 
-    req.on('data', (data) => {
-      dataString += data
+    form.on('part', (part) => {
+      if (part.filename) {
+        let dataString = ''
+
+        part.setEncoding('binary')
+        part.on('data', (data) => {
+          dataString += data
+        })
+        part.on('end', () => {
+          let fileName = shortid.generate()
+          let filePath = `content/images/${fileName}`
+
+          product.image = filePath
+          fs.writeFile(`${filePath}`, dataString, {encoding: 'ascii'}, (err) => {
+            if (err) {
+              console.log(err)
+            }
+          })
+        })
+      } else {
+        part.setEncoding('utf-8')
+        let field = ''
+        part.on('data', (data) => {
+          field += data
+        })
+
+        part.on('end', () => {
+          product[part.name] = field
+        })
+      }
     })
 
-    req.on('end', () => {
-      let product = qs.parse(dataString)
+    form.on('close', () => {
       database.products.add(product)
-
       res.writeHead(302, {
         Location: '/'
       })
-
       res.end()
     })
+
+    form.parse(req)
   } else {
     return true
   }
