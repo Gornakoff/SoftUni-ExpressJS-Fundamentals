7518210eafee4efdb83e7c6c3b05b75f8803a04c
nikolaygornakov <nikolaygornakov@yahoo.com>
2017-06-07 17:07:25 +0300

    add passport configuration

--- Architecture-and-Authentication/server/config/express.js
+++ Architecture-and-Authentication/server/config/express.js
@@ -1,5 +1,9 @@
 const handlebars = require('express-handlebars')
 const express = require('express')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const passport = require('passport')
 
 module.exports = (app) => {
   app.engine('handlebars', handlebars({
@@ -7,6 +11,17 @@ module.exports = (app) => {
   }))
 
   app.set('view engine', 'handlebars')
+
+  app.use(cookieParser())
+  app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(session({
+    secret: 'something-secret!@#$$$@',
+    resave: false,
+    saveUninitialized: false
+  }))
+  app.use(passport.initialize())
+  app.use(passport.session())
+
   app.use(express.static('public'))
 
   console.log('Express ready!')
