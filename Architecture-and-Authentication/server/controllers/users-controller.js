6cb7853d49f643f3da9de9b8999c56ba3c67079f
nikolaygornakov <nikolaygornakov@yahoo.com>
2017-06-07 18:14:48 +0300

    add User register, login, logout

---
+++ Architecture-and-Authentication/server/controllers/users-controller.js
@@ -0,0 +1,66 @@
+const encryption = require('../utilities/encryption')
+const User = require('../data/User')
+
+module.exports = {
+  registerGet: (req, res) => {
+    res.render('users/register')
+  },
+  registerPost: (req, res) => {
+    let reqUser = req.body
+    // add validations
+    let salt = encryption.generateSalt()
+    let hashedPassword = encryption.generateHashedPassword(salt, reqUser.password)
+
+    let newUser = {
+      username: reqUser.username,
+      firstName: reqUser.firstName,
+      lastName: reqUser.lastName,
+      salt: salt,
+      hashedPass: hashedPassword
+    }
+
+    User.create(newUser).then((user) => {
+      req.logIn(user, (err, user) => {
+        if (err) {
+          res.locals.globalError = err
+          res.render('users/register', user)
+        }
+
+        res.redirect('/')
+      })
+    })
+  },
+  loginGet: (req, res) => {
+    res.render('users/login')
+  },
+  loginPost: (req, res) => {
+    let reqUser = req.body
+
+    User.findOne({username: reqUser.username}).then((user) => {
+      if (!user) {
+        res.locals.globalError = 'Invalid user data'
+        res.render('users/login')
+        return
+      }
+      if (!user.authenticate(reqUser.password)) {
+        res.locals.globalError = 'Invalid user data'
+        res.render('user/login')
+        return
+      }
+
+      req.login(user, (err, user) => {
+        if (err) {
+          res.locals.globalError = err
+          res.render('users/login')
+          return
+        }
+
+        res.redirect('/')
+      })
+    })
+  },
+  logout: (req, res) => {
+    req.logout()
+    res.redirect('/')
+  }
+}
