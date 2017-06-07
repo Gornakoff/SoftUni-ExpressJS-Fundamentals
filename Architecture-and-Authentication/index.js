fbe68718e159490a14f91ebbdab56a7fb72f6e92
nikolaygornakov <nikolaygornakov@yahoo.com>
2017-06-07 15:38:13 +0300

    Configure server settings

--- Architecture-and-Authentication/index.js
+++ Architecture-and-Authentication/index.js
@@ -1,20 +1,11 @@
 const express = require('express')
-const mongoose = require('mongoose')
-const dbName = 'boilerplate'
-const port = process.env.PORT || 1337
 const env = process.env.NODE_ENV || 'development'
-
-mongoose.Promise = global.Promise
+const settings = require('./server/config/settings')[env]
 
 let app = express()
+require('./server/config/database')(settings)
+require('./server/config/express')(app)
+require('./server/config/routes')(app)
 
-app.get('/', (req, res) => {
-  mongoose.connect(`mongodb://localhost:27017/${dbName}`)
-    .then(() => {
-      console.log('Connected to database!')
-    })
-  res.send('Hello!')
-})
-
-app.listen(port)
-console.log(`Server is listening on port ${port}...`)
+app.listen(settings.port)
+console.log(`Server is listening on port ${settings.port}...`)
---
+++ Architecture-and-Authentication/server/config/settings.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const dbName = 'boilerplate'
+const port = process.env.PORT || 1337
+
+let rootPath = path.normalize(path.join(__dirname, '/../../'))
+
+module.exports = {
+  development: {
+    rootPath: rootPath,
+    db: `mongodb://localhost:27017/${dbName}`,
+    port: port
+  },
+  staging: {
+  },
+  production: {
+  }
+}
