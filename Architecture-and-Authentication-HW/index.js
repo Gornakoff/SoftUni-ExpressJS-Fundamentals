a201731f4a0914c8e89fa51eadb4e4cf4506d191
nikolaygornakov <nikolaygornakov@yahoo.com>
2017-06-08 02:18:15 +0300

    added user authentication

--- Architecture-and-Authentication-HW/index.js
+++ Architecture-and-Authentication-HW/index.js
@@ -9,6 +9,7 @@ let environment = process.env.NODE_environment || 'development'
 database(config[environment])
 require('./config/express')(app, config[environment])
 require('./config/routes')(app)
+require('./config/passport')()
 
 app.listen(port)
 
