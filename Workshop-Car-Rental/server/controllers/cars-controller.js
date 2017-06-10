423af386476d41d9279e99babaf8cde177575f39
nikolaygornakov <nikolaygornakov@yahoo.com>
2017-06-10 04:09:24 +0300

    fix paging

--- Workshop-Car-Rental/server/controllers/cars-controller.js
+++ Workshop-Car-Rental/server/controllers/cars-controller.js
@@ -36,28 +36,42 @@ module.exports = {
   all: (req, res) => {
     const sortByPropName = 'createdOn'
 
+    let pagesCount = 0
     let pageSize = 1
     let page = parseInt(req.query.page) || 1
     let search = req.query.search
 
     let query = Car.find({})
+    let regexSearch = new RegExp(search, 'i') // matching case insensitive text
+
     if (search) {
-      query = query.where('make').regex(new RegExp(search, 'i')) // case insensitive search
+      // query = query.where('make').regex(regexSearch) // search in one location
+      query = query.or([{ make: regexSearch }, { model: regexSearch }]) // search in multiple locations
     }
 
-    query
-      .sort(`-${sortByPropName}`) // sort by property( createdOn ) in reverse order (using '-' symbol)
-      .skip((page - 1) * pageSize)
-      .limit(pageSize)
-      .then((cars) => {
-        res.render('cars/all', {
-          cars: cars, // set variable for handlebars
-          hasPrevPage: page > 1,
-          hasNextPage: cars.length > 0,
-          prevPage: page - 1,
-          nextPage: page + 1,
-          search: search
+    query.then(carsData => {
+      pagesCount = Math.ceil(carsData.length / pageSize)
+
+      query
+        .sort(sortByPropName)
+        // .sort(`-${sortByPropName}`) // sort by property( createdOn ) in reverse order (using '-' symbol)
+        .skip((page - 1) * pageSize)
+        .limit(pageSize)
+        .then((cars) => {
+          res.render('cars/all', {
+            cars: cars, // set variable for handlebars
+            hasPrevPage: page > 1,
+            hasNextPage: page < pagesCount,
+            // hasNextPage: cars.length > 0,
+            currentPage: page,
+            prevPage: page - 1,
+            nextPage: page + 1,
+            isNotFirst: page > 2,
+            isNotLast: page < pagesCount - 1,
+            lastPage: pagesCount,
+            search: search
+          })
         })
-      })
+    })
   }
 }
